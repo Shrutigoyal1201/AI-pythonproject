fb4c89457e871750176dc2027562bf4b456bf8a6
Shrutigoyal1201 <73349120+Shrutigoyal1201@users.noreply.github.com>
2021-08-29 23:16:40 +0530

    Update MAJOR-PROJECT.py

--- MAJOR-PROJECT.py
+++ MAJOR-PROJECT.py
@@ -224,69 +224,6 @@ def dateTime():
     
     canvas.mainloop()
 
-
-def Calculator():
-
-    cal= Tk()
-    cal.geometry("270x570")
-    cal.title("Calculator")
-    # cal.call('wm', 'iconphoto', cal._w, PhotoImage(file='cal.png'))  #icon
-
-    scvalue= StringVar()
-    scvalue.set("")
-    screen= Entry(cal,textvar=scvalue, font="lucida 30 bold")
-    screen.pack(fill=X,padx=10,pady=10,ipadx=10)
-
-    def click(event):
-        
-        text= event.widget.cget("text")  #event.widget=> gives button that has been clicked::: .cget("text")=> gives the text on the button
-        print(text)  #print on click (on terminal)  #optional
-
-        if text== "=":
-            if scvalue.get().isdigit():
-                value= int(scvalue.get())   #typecasting to int if it is adigit
-            else:    #if it is not adigit but an expression as 2x3
-                try:
-                    value= eval(screen.get())  #eval func evaluates a string
-                except Exception as e:
-                    value= "Error !"
-                    
-            scvalue.set(value)     #seting value in scvalue
-           
-        elif text== "C":
-            scvalue.set("")
-           
-        else:   #if it is a number
-            scvalue.set(scvalue.get() + text)#setting sc value again
-        
-        screen.update()  #will update entry at screen with the new value every time
-
-
-    l=[['9','8','7'],['6','5','4'],['3','2','1'],[['0','grey'],['-','black'],['+','black']],[['*','black'],['/','black'],['%','black']],[['.','black'],['=','green'],['C','red']]]
-        
-    for i in range(len(l)):
-        
-        #making a frame
-        f= Frame(cal, bg="black")  
-          
-        for j in range(len(l[i])):
-            
-            if i>=3:
-                
-                b= Button(f, text=l[i][j][0], padx=15, bg=l[i][j][1], fg="white", pady=10, font="lucida 20 bold",relief="raise") 
-                b.bind("<Button-1>", click)
-                b.pack(side=LEFT, padx=5, pady=4)
-                    
-            else:
-                #button inside frame f =>Button(f,)
-                b= Button(f, text=l[i][j], padx=15, pady=10, bg="grey", fg="white",  font="lucida 20 bold",relief="raise")   
-                #binding button to a click event before packing
-                b.bind("<Button-1>", click)  
-                b.pack(side=LEFT, padx=5, pady=4)
-
-            # packing frame
-            f.pack()     
-    
 def mic():
     r=sr.Recognizer() # recognizes the sound
 
